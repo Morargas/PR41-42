b96c18912ee11c81fdd8f398f7cb5dfaee264012
Morargas <139104004+Morargas@users.noreply.github.com>
2026-06-06 12:57:11 +0700

    Update messageService.js

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,6 +1,6 @@
 import prisma from '../prisma/prismaClient.js';
 import AppError from '../utils/appError.js';
-
+//Функции для работы с Prisma (с соббщениями)
 async function getUserBySupabaseId(supabaseId) {
     const user = await prisma.user.findUnique({ where: { supabaseId } });
     if (!user) throw new AppError('Пользователь не найден', 404);
