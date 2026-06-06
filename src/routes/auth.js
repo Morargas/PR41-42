038ada0082b4585002618d389e424aee651f46ba
Morargas <139104004+Morargas@users.noreply.github.com>
2026-06-06 12:52:25 +0700

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import * as authController from '../controllers/authController.js';
 import authenticate from '../middleware/authenticate.js';
 import { validate, registerSchema, loginSchema } from '../validators/auth.js';
-
+//Создание маршрутов
 const router = Router();
 
 router.post('/register', validate(registerSchema), authController.register);
