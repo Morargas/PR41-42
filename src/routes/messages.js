eeca08b43c57c682ecbc9fb90ac9d49fc109e32f
Morargas <139104004+Morargas@users.noreply.github.com>
2026-06-06 12:53:39 +0700

    Update messages.js

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -3,7 +3,7 @@ import * as messageController from '../controllers/messageController.js'
 import authenticate from '../middleware/authenticate.js'
 import { validate } from '../validators/auth.js'
 import { createMessageSchema } from '../validators/message.js'
-
+//Маршруты для сообщений (получения (get) и отправки (post))
 const router = Router()
 
 router.get('/:id/messages', authenticate, messageController.getMessages)
